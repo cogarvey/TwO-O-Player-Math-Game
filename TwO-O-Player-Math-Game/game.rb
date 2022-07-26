9cebc662f6b0f16a5bea935c009d80212ba453ea
Colleen Garvey <colleenellengarvey@gmail.com>
2022-07-26 10:23:51 -0400

    complete

---
+++ TwO-O-Player-Math-Game/game.rb
@@ -0,0 +1,30 @@
+class Game
+  
+  attr_reader :players, :current_player
+  
+  def initialize(player1, player2)
+    @players=[player1.name, player2.name]
+    @current_player = @players[0]
+    @player1 = player1
+    @player2 = player2
+  end
+
+  def defeat(player)
+
+    if player == @player1.name
+      @player1.score = @player1.score - 1
+    elsif player == @player2.name
+      @player2.score = @player2.score - 1
+    end
+
+  end
+
+  def change_player
+    if @current_player == @players[0]
+      @current_player = @players[1]
+    elsif
+      @current_player = @players[0]
+    end
+  end
+
+end
---
+++ TwO-O-Player-Math-Game/main.rb
@@ -0,0 +1,45 @@
+# Required files
+require './question'
+require './player'
+require './game'
+
+question = Question.new
+player1 = Player.new("Player1")
+player2 = Player.new("Player2")
+game = Game.new(player1, player2)
+
+while player1.score > 0 && player2.score > 0
+
+  # ask player question
+  question.random_addition_question(game.current_player)
+
+  # player input answer and store as variable
+  input = gets.chomp.to_i
+
+  # respond with text correct/wrong
+  if input == question.ans
+    puts "#{game.current_player}: Correct!"
+  else
+    game.defeat(game.current_player)
+    puts "#{game.current_player}: Incorrect!"
+  end
+
+  # output scores
+  puts "P1: #{player1.score}/3 vs P2: #{player2.score}/3"
+
+  # new turn
+  puts "----- NEW TURN -------"
+
+  # Change player
+  game.change_player
+
+  # end game if points == 0
+  if player1.score == 0
+    puts "Player 2 wins with a score of #{player2.score}/3"
+    puts "----- GAME OVER -------"
+  end
+  if player2.score == 0
+    puts "Player 1 wins with a score of #{player1.score}/3"
+    puts "----- GAME OVER -------"
+  end
+end
